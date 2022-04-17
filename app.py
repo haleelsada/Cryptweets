306fffa4f14b534d15a3c095d807ac67dcc5c42c
Haleelsada <75977159+haleelsada@users.noreply.github.com>
2022-04-18 00:03:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.header("Using Twitter API to predict future movement of cryptocurrencies and 
 test='It is known that social media platforms like twitter plays an imortant role in determining the movement of financial asset platforms like stock market and crytpocurrencies. It is expected to increase the price of Doge after a tweet from Elon musk. Here the sentiment of public about a cryptocoin or stock (or any current issue going on!) could be calculated with the help of twitter api and huggingface transformers and a little mathematics'
 
 st.subheader(test)
-key = st.text_input(label="Enter the keyword    eg:bitcoin ",)
+key = st.text_input(label="Enter the keyword    eg: bitcoin/ nifty50/ tesla ",)
 
 while key!='':
     st.subheader(cryptweet.sentimentanalyser(key))
