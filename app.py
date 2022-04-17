9ca6d92c84525432fa4e2f42a2988ac4aefce338
Haleelsada <75977159+haleelsada@users.noreply.github.com>
2022-04-17 23:17:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,7 @@ st.set_page_config(page_title="Cryptweets",page_icon='https://preview.pixlr.com/
 
 st.title('Cryptweets')
 st.header("Using Twitter API to predict future movement of cryptocurrencies and stocks")
-test='''It is known that social media platforms like twitter plays an imortant role in
- determining the price of financial asset platforms like stock market and crytpocurrencies. It is 
- expected to increase the price of Doge after a tweet from Elon musk. Here the sentiment of public
-  about a cryptocoin or stock (or any current issue going on!) could be calculated with the help of 
-  twitter api and huggingface transformers and a little mathematics'''
+test='It is known that social media platforms like twitter plays an imortant role in determining the movement of financial asset platforms like stock market and crytpocurrencies. It is expected to increase the price of Doge after a tweet from Elon musk. Here the sentiment of public about a cryptocoin or stock (or any current issue going on!) could be calculated with the help of twitter api and huggingface transformers and a little mathematics'
 
 st.subheader(test)
 key = st.text_input(label="Enter the keyword eg:#bitcoin ",)
